057a937402d0e6993a56ddb7e94a0a510826a80a
John Allwine <john@pocketnc.com>
2025-03-12 16:23:05 -0600

    install rsync, reorder ostree apt-get install to happen before building our version SOFT-1602

--- docker/build-ostree.sh
+++ docker/build-ostree.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "deb-src https://deb.debian.org/debian bookworm main contrib non-free non-free-firmware" >> /etc/apt/sources.list
-apt-get update
+apt-get update -y
 
 apt-get -y install build-essential 
 apt-get -y build-dep ostree
 
 cd /tmp
-git clone https://github.com/pocketnc/ostree
+git clone --single-branch --branch bookworm https://github.com/pocketnc/ostree
 
 cd ostree
 git submodule update --init
