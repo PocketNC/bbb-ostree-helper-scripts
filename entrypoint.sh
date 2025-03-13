359631d0587965bd21d9e86566ee6ba1ca5a6242
John Allwine <john@pocketnc.com>
2025-03-13 11:43:28 -0600

    added custom_prep and custom_finalize scripts so external processes can add extra functionality SOFT-1602

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,13 @@ export IMG=/tmp/$(basename $INPUT_IMG)
 export OUTPUT_IMG=/host/$(basename $INPUT_IMG .img)-ostree.img
 
 . /host/prepare_builddir.sh
+if [ -f /host/custom_prep.sh ]; then
+  . /host/custom_prep.sh
+fi
 . /host/install_dracut_and_ostree.sh
 . /host/ostree_prep_rootfs.sh
 . /host/create_ostree_rootfs.sh
+if [ -f /host/custom_finalize.sh ]; then
+  . /host/custom_finalize.sh
+fi
 . /host/cleanup.sh
